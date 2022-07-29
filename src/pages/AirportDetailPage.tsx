f29ac94556093ce83b2a4e3fbc931a349d678c95
AleksandrFROLik <nextfrolik@gmail.com>
2022-07-29 12:02:06 +0300

    update component AirportDetail

--- src/pages/AirportDetailPage.tsx
+++ src/pages/AirportDetailPage.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import axios from "../axios";
+import {IAirportDetail} from "../models/models";
+
 
 export const AirportDetailPage = () => {
+
+    const [airportDetail, setAirportDetail] = useState<IAirportDetail | null>(null)
+    const [loading, setLoading] = useState(false)
     const params = useParams<'id'>()
 
+    const fetchAirportDetail = async () => {
+        setLoading(true)
+        const response = await axios.get<IAirportDetail>(`airports/${params.id}`)
+        setAirportDetail(response.data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchAirportDetail()
+    }, [])
+
+    if (loading) return <p className='text-center text-2xl color-yellow'>Loading...</p>
     return (
         <div className='container mx-auto pt-5 max-w-[760px]'>
-            Airport{params.id}
+            <h1 className='text-center text-2xl'>{airportDetail?.name}</h1>
         </div>
     );
 };
