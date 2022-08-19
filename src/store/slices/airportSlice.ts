cb6e67e555ddc8ec859145ca3af9e5d62c5b095f
AleksandrFROLik <nextfrolik@gmail.com>
2022-08-19 22:39:50 +0300

    create redux++

--- src/store/slices/airportSlice.ts
+++ src/store/slices/airportSlice.ts
@@ -1,54 +1,54 @@
-import {IAirport, IFilter} from "../../models/models";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { IAirport, IFilter } from "../../models/models";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 
 interface AirportSlice {
-    loading: boolean
-    error: string
-    airports: IAirport[]
-    airportsContainer: IAirport[]
-    count: number
+  loading: boolean
+  error: string
+  airports: IAirport[]
+  airportsContainer: IAirport[]
+  count: number
 }
 
 interface AirportPayload {
-    airports: IAirport[]
-    count: number
+  airports: IAirport[]
+  count: number
 }
 
 
 const initialState: AirportSlice = {
-    loading: false,
-    error: '',
-    airports: [],
-    airportsContainer: [],
-    count: 0
+  loading: false,
+  error: '',
+  airports: [],
+  airportsContainer: [],
+  count: 0
 }
 
 export const airportSlice = createSlice({
-    name: 'airport',
-    initialState: initialState,
-    reducers: {
-        fetching(state) {
-            state.loading = true
-        },
-        fetchSuccess(state, action: PayloadAction<AirportPayload>) {
-            state.loading = false
-            state.airports = action.payload.airports
-            state.airportsContainer = action.payload.airports
-            state.count = action.payload.count
-            state.error = ''
-        },
-        fetchError(state, action: PayloadAction<Error>) {
-            state.loading = false
-            state.error = action.payload.message
-        },
-        fetchFilterAirports(state, action: PayloadAction<IFilter>) {
-            state.airports = state.airportsContainer
-                .filter(airport => airport.type.includes(action.payload.type))
-                .filter(airport => airport.region.includes(action.payload.region))
-                .filter(airport => airport.country.includes(action.payload.country))
-        }
+  name: 'airport',
+  initialState: initialState,
+  reducers: {
+    fetching(state) {
+      state.loading = true
+    },
+    fetchSuccess(state, action: PayloadAction<AirportPayload>) {
+      state.loading = false
+      state.airports = action.payload.airports
+      state.airportsContainer = action.payload.airports
+      state.count = action.payload.count
+      state.error = ''
+    },
+    fetchError(state, action: PayloadAction<Error>) {
+      state.loading = false
+      state.error = action.payload.message
+    },
+    fetchFilterAirports(state, action: PayloadAction<IFilter>) {
+      state.airports = state.airportsContainer
+                            .filter(airport => airport.type.includes(action.payload.type))
+                            .filter(airport => airport.region.includes(action.payload.region))
+                            .filter(airport => airport.country.includes(action.payload.country))
     }
+  }
 })
 
 export default airportSlice.reducer
