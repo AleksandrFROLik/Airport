a1d03279106de6558309d2c8c46a32a583963f73
AleksandrFROLik <nextfrolik@gmail.com>
2022-10-08 12:07:42 +0300

    create redux path 1

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,18 +1,18 @@
 export interface IAirport {
-    id: number
-    name: string
-    ident: string
-    local_code: string
-    region: string
-    type: string
-    country: string
+  id: number
+  name: string
+  ident: string
+  local_code: string
+  region: string
+  type: string
+  country: string
 }
 
 export interface ServerResponse<T> {
-    count: number
-    next: null | number
-    previous: null | number
-    results: T[]
+  count: number
+  next: null | number
+  previous: null | number
+  results: T[]
 }
 
 export type IAirportType = string
@@ -20,27 +20,27 @@ export type IAirportRegion = string
 export type IAirportCountry = string
 
 export interface IFilter {
-    type: IAirportType
-    region: IAirportRegion
-    country: IAirportCountry
+  type: IAirportType
+  region: IAirportRegion
+  country: IAirportCountry
 }
 
 export interface IAirportDetail {
-    continent:string
-    coordinates: string
-    country: string
-    elevation_ft: string
-    gps_code: string
-    iata_code: null | string
-    ident: string
-    local_code: null | string
-    municipality: string
-    name: string
-    region: string
-    type: string
+  continent: string
+  coordinates: string
+  country: string
+  elevation_ft: string
+  gps_code: string
+  iata_code: null | string
+  ident: string
+  local_code: null | string
+  municipality: string
+  name: string
+  region: string
+  type: string
 }
 
 export interface IAuth {
-    username:string
-    password:string
+  username: string
+  password: string
 }
